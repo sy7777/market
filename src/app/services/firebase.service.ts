4499ef9e14c8a46de17442435b23b92fcab625ec
sy7 <piecesy7@gmail.com>
2021-03-08 14:48:27 +1000

    saved

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
 
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  private fireStore: firebase.firestore.Firestore;
+  private fireStorage: firebase.storage.Storage;
+  // Initialize Firebase
 
   constructor() { }
 }
